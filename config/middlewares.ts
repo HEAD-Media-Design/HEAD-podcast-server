4e926395ba14311fe5a82f4d3774d4338547ebf0
Haneul Lee <lovesky4294@gmail.com>
2025-04-25 13:40:43 +0200

    fix: fix not showing media

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,29 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://res.cloudinary.com",
+          ],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com"],
+          "connect-src": ["'self'", "https://res.cloudinary.com"],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
